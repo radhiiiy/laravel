b9f588c83cd5260ea1eacd142462aca27f173e06
radhiiiy <radhiyyan2322gmail.com>
2026-02-02 15:14:34 +0700

    add

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,12 +7,13 @@
     <title>CRUD</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous" defer></script>
+    <link rel="stylesheet" href="/public/assets/style.css">
 </head>
 <body class="bg-light">
-    <div class="container mt-4">
+    <div class="container">
         @yield('content')
-        <a href="/siswa/create" class="btn btn-primary mb-3">Tambah Siswa</a>
-    </div>
 
+    </div>
+<a href="/siswa/create" class="btn btn-primary mb-3">Tambah Siswa</a>
 </body>
 </html>
